43e0a32368ead72331b55eb84b6fd2f4eebd3c38
BritnessDev <britness.gmd@gmail.com>
2023-08-29 19:24:01 -0700

    feat: init project

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface IBannerButton {
+  text: string,
+  onClickHandler: () => void
+}
+
+export const NormalButton: React.FC<IBannerButton> = ({ text, onClickHandler }) => (
+    <button className="text-red-400" onClick={onClickHandler}>
+        {text}
+    </button>
+)
