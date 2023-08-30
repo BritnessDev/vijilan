cc7ba2728cc5bc6f075f28c6d08d322e24ee518e
naturalspirit-dev <natural.spirit0102@gmail.com>
2023-08-30 16:53:11 +0800

    feat: check email

--- src/pages/sign-up/check-email.tsx
+++ src/pages/sign-up/check-email.tsx
@@ -3,16 +3,14 @@ import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 function CheckEmail() {
     return (
-        <div className='bg-gray-500 flex items-center h-screen'>
-            <div className='mx-auto my-auto login-block bg-white max-w-lg'>
-                <div className='py-16 px-[100px]'>
-                    <div className='px-16'>
-                        <div className='flex justify-center'>
-                            <CheckCircleOutlineIcon className='text-3xl text-[#40B17B]' />
+        <div className="bg-gray-500 flex items-center h-screen">
+            <div className="mx-auto my-auto login-block bg-white max-w-lg">
+                <div className="py-16 px-[100px]">
+                    <div className="px-16">
+                        <div className="flex justify-center">
+                            <CheckCircleOutlineIcon className="text-3xl text-[#40B17B]" />
                         </div>
-                        <div>
-                            Your registration was successfull!
-                        </div>
+                        <div>Your registration was successfull!</div>
                         Please, check your email to activate your account.
                     </div>
                 </div>
