ebc75e9274c41f696785a8dcb3163c822ddd342e
BritnessDev <britness.gmd@gmail.com>
2023-08-30 00:07:02 -0700

    feat: add react router

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'themeBg1': '#F1F2F5'
+            },
+            boxShadow: {
+                card: '0px 0px 5px 0px rgba(0, 0, 0, 0.25)',
+            },
+        },
     },
     plugins: [],
 };
